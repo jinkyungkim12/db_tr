c0f762adc45fedc24339bb8ebdee4709b0739976
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-05 12:39:47 +0900

    up

--- mysql/sql/basic1_2.sql
+++ mysql/sql/basic1_2.sql
@@ -1119,4 +1119,3 @@ select * from transport;
 
 
 
-
--- mysql/sql/jk_project.sql
+++ mysql/sql/jk_project.sql
@@ -33,13 +33,111 @@ select * from transport where address like "%서울시 마포구%";
 select * from transport where addressDetail like "%아파트%";
 
 
--- 회원가입
+-- member 생년월일, 성별, 주소, 주소디테일
 
-select * from where
+select
+	 a.seq
+    ,a.dob
+    ,a.gender
+    ,b.seq
+    ,b.address
+    ,b.addressDetail
+from member a
+ left join transport b on b.member_seq = a.seq
+-- inner join review b on b.classProduct_seq = a.seq
+-- join review b on b.classProduct_seq = a.seq
+;
+
+-- member 생년월일 1989년, 성별 남성, 주소 서울시 사는 사람
+
+select
+	 a.seq
+    ,a.name
+    ,a.dob
+    ,a.gender
+    ,b.member_seq
+    ,b.address
+  from member a
+ left join transport b on b.member_seq = a.seq
+-- inner join review b on b.classProduct_seq = a.seq
+-- join review b on b.classProduct_seq = a.seq
+where 1=1
+and a.dob like "1989%"
+and a.gender like 4
+and b.address like "서울시%"
+;
+
+-- member 서울사는 크리에이터 여성
+
+select 
+	a.seq
+    ,a.name
+    ,a.gender
+    ,a.position
+    ,b.member_seq
+    ,b.address
+  from member a
+  left join transport b on b.member_seq = a.seq
+  
+  where
+  1=1
+	and a.gender like 5
+	and a.position like 23
+    and b.address like "서울%"
+  ;
 
 
+-- member 서울시 마포구 사는 수강생 남성
 
+select 
+	a.seq
+    ,a.name
+    ,a.gender
+    ,a.position
+    ,b.member_seq
+    ,b.address
+  from member a
+  left join transport b on b.member_seq = a.seq
+  
+  where
+  1=1
+	and a.gender like 4
+	and a.position like 22
+    and b.address like "서울시 마포구%"
+  ;
 
+-- member : skt 쓰는 마포구 사는 사람
 
+select 
+	 a.seq
+	,a.name
+    ,a.telCompany
+    ,b.member_seq
+    ,b.address
+from member a 
+left join transport b on b.member_seq = a.seq
 
--- 
+where
+1=1
+and a.telCompany like 1
+and b.address like "%마포구%" 
+;
+
+-- member 서울시에 사는 90년대 생들
+
+select
+	 a.seq
+	,a.name
+    ,a.gender
+    ,a.dob
+    ,b.member_seq
+    ,b.address
+ from member a
+ left join transport b on b.member_seq = a.seq
+ 
+ where
+ 1=1
+ and a.dob like "&99&"
+ and b.address like "서울시%"
+ ;
+
