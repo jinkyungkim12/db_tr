77fbfc2c11b522cdf6fc5e6d394344547203ad17
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-08 11:54:17 +0900

    up

--- mysql/sql/jk_project.sql
+++ mysql/sql/jk_project.sql
@@ -14,25 +14,87 @@ from CommonCodeGroup a
 ;
 
 
--- 로그인
+-- 로그인/ 아이디, 비번 찾기
 
 select
-from member a
- where id="kangsong" and password="songsasda";
-select * from member where id="suzybae" and password="suzy121235yzus";
-select * from member where id="bogum" and password="qhrja57865";
+	name
+    ,id
+    ,password
+from member 
+where id="kangsong" and password="songsasda";
+
+
+select
+	name
+    ,id
+    ,password
+from member 
+where id="suzybae" and password="suzy121235yzus";
+
+select
+	name
+    ,id
+    ,password
+from member 
+where id="bogum" and password="qhrja57865";
 
 -- 배송 zipcode: 30512, 10570, 78974
 
-select * from transport where zipcode="30512";
-select * from transport where zipcode="10570";
-select * from transport where zipcode="78974";
+select
+	a.seq
+	,a.name
+    ,a.id
+    ,b.member_seq
+    ,b.zipcode
+from member a
+left join transport b on b.member_seq = a.seq
+where 
+1=1
+and b.zipcode like "30512"
+;
+select
+	a.seq
+	,a.name
+    ,a.id
+    ,b.member_seq
+    ,b.zipcode
+from member a
+left join transport b on b.member_seq = a.seq
+where 
+1=1
+and b.zipcode like "10570"
+;
 
--- 배송지: 강남구/ 마포구/ 아파트
+select
+	a.seq
+	,a.name
+    ,a.id
+    ,b.member_seq
+    ,b.zipcode
+from member a
+left join transport b on b.member_seq = a.seq
+where 
+1=1
+and b.zipcode like "78974"
+;
 
-select * from transport where address like "%서울시 강남구%";
-select * from transport where address like "%서울시 마포구%";
-select * from transport where addressDetail like "%아파트%";
+
+-- 배송지: 강남구, 아파트 사는 사람
+
+select 
+	a.seq
+    ,a.name
+    ,a.id
+    ,b.member_seq
+    ,b.address
+    ,b.addressDetail
+from member a
+left join transport b on b.member_seq = a.seq
+where
+1=1
+and b.address like "%서울시 강남구%"
+and b.addressDetail like "%아파트%"
+;
 
 
 -- member 생년월일, 성별, 주소, 주소디테일
